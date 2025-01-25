6360429e7f0c5a1ade11562c117b1ef3dc35ed97
BitPod.AI <BitPodAI@gmail.com>
2025-01-25 12:05:49 +0800

    Update Sol Spl

--- packages/plugin-data-enrich/src/solanaspl.ts
+++ packages/plugin-data-enrich/src/solanaspl.ts
@@ -4,7 +4,12 @@ import {
     PublicKey,
     Transaction,
 } from "@solana/web3.js";
-import { TOKEN_PROGRAM_ID, createTransferInstruction } from "@solana/spl-token";
+import {
+    TOKEN_PROGRAM_ID,
+    createTransferInstruction,
+    getAssociatedTokenAddress
+} from "@solana/spl-token";
+import { settings } from "@ai16z/eliza";
 
 interface TransferTokenParams {
     fromTokenAccountPubkey: PublicKey | string;
@@ -21,7 +26,7 @@ export class InvalidPublicKeyError extends Error {
 }
 
 /**
- * Create ai16z Meme Coin Transacation
+ * Create Solana SPL Meme Coin Transacation
  * @param params Trans input
  * @returns Transaction Output
  */
@@ -31,37 +36,57 @@ export async function createSolSplTransferTransaction({
     ownerPubkey,
     tokenAmount,
 }: TransferTokenParams): Promise<Transaction> {
-    const connection = new Connection(
-        clusterApiUrl("mainnet-beta"),
-        "confirmed"
-    );
-
-    // Check the address
-    const fromTokenAccount = new PublicKey(fromTokenAccountPubkey);
-    const toTokenAccount = new PublicKey(toTokenAccountPubkey);
-    const owner = new PublicKey(ownerPubkey);
-
-    // Create Trans
-    const transaction = new Transaction();
-
-    // Add SPL
-    transaction.add(
-        createTransferInstruction(
+    try {
+        const connection = new Connection(
+            clusterApiUrl("mainnet-beta"),
+            "confirmed"
+        );
+    
+        // Check the address
+        const fromTokenAccount = new PublicKey(fromTokenAccountPubkey);
+        const toTokenAccount = new PublicKey(toTokenAccountPubkey);
+        const owner = new PublicKey(ownerPubkey);
+        const mint = new PublicKey(settings.SOL_SPL_MINT_PUBKEY);
+    
+        // Get the soure SPL SmartContract Address (if exist)
+        const sourceTokenAccount = await getAssociatedTokenAddress(
             fromTokenAccount,
+            mint,
+            false
+        );
+    
+        // Get the dest SPL SmartContract Address (if exist)
+        const destinationTokenAccount = await getAssociatedTokenAddress(
             toTokenAccount,
-            owner,
-            tokenAmount,
-            [],
-            TOKEN_PROGRAM_ID
-        )
-    );
-
-    // Set Gas Address
-    transaction.feePayer = owner;
-
-    // Get Result
-    const { blockhash } = await connection.getLatestBlockhash();
-    transaction.recentBlockhash = blockhash;
-
-    return transaction;
+            mint,
+            false
+        );
+    
+        // Create Trans
+        const transaction = new Transaction();
+    
+        // Add SPL
+        transaction.add(
+            createTransferInstruction(
+                sourceTokenAccount,
+                destinationTokenAccount,
+                owner,
+                tokenAmount,
+                [],
+                TOKEN_PROGRAM_ID
+            )
+        );
+    
+        // Set Gas Address
+        transaction.feePayer = owner;
+    
+        // Get Result
+        const { blockhash } = await connection.getLatestBlockhash();
+        transaction.recentBlockhash = blockhash;
+    
+        return transaction;
+    } catch (err) {
+        console.error(err);
+        throw new Error("SPL Transaction Error.");
+    }
 }
