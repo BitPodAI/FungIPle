211a434b273efa1b14e41a0a3f99befadcfca149
BitPod <BitPodAI@gmail.com>
2025-02-22 06:07:07 +0800

    Update the twitter summary

--- packages/plugin-data-enrich/src/social.ts
+++ packages/plugin-data-enrich/src/social.ts
@@ -89,9 +89,10 @@ export const socialProvider: Provider = {
 }
 
 const ABSTRACTOR_INSTRUCTION = `
-    Please summary the user information by the provided biography and post tweets.
+    Please summary the twitter account information by the provided biography and post tweets.
     The total words count should be between 20 and 30.
-    If the user is not related to web3 or crypto, just return "The user is not related to Web3".`;
+    The summary result should not contain the words 'user'/'account', use their original names instead.
+    If the account is not related to web3 or crypto, just return "The item is not related to Web3".`;
 
 const TW_ABSTRACTOR_PREFIX: string = "ABSTRACTOR_KEY_TW_PROFILE_PREFIX_";
 const TW_ABSTRACTOR_POST_COUNT = 20;
@@ -159,7 +160,7 @@ export class twitterDataProvider {
             });
             //console.log(response);
             summary = response;
-            if (response && response.includes("The user is not related to Web3")) {
+            if (response && response.includes("The item is not related to Web3")) {
                 summary = "";
             }
         } catch (error) {
